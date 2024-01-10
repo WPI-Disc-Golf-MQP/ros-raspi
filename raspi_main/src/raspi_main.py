500a1038f78eaddba11319513fbad3224a75728f
Benjamin A <yellowjaguar5@gmail.com>
2024-01-10 10:08:28 -0500

    add button test

--- raspi_main/src/raspi_main.py
+++ raspi_main/src/raspi_main.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python3
 
-import ui
+#from raspi_ui import ui
 
 import rospy
+from std_msgs.msg import String
 
 class raspi_main:
     def __init__(self):
         rospy.init_node('raspi_main')
         rospy.loginfo("raspi_main node started")
-        
-        self.ui = raspi_ui.ui()
 
         # --- Subscribers ---
 
         # --- Publishers ---
+
+        self.button_b_subscriber = rospy.Subscriber('button_b', String, self.button_b_callback)
+
+    def button_b_callback(self, msg):
+        print("[main]Button B Pressed")
     
     def run(self):
         """
         Runs the node until Ctrl-C is pressed.
         """
         rospy.spin()
+
+if __name__ == '__main__':
+    raspi_main = raspi_main()
+    raspi_main.run()
--- raspi_main/src/raspi_ui.py
+++ raspi_main/src/raspi_ui.py
@@ -1,39 +1,44 @@
 #!/usr/bin/env python3
-ch
 
 import rospy
 import customtkinter as ctk
 import tkinter as tk
 from tkinter import ttk
 
+from std_msgs.msg import String
+
 class ui:
-    rospy.init_node('raspi_ui')
-    rospy.loginfo("raspi_ui node started")
-
-    ros_publish_test = rospy.Publisher('ui_publish_test', Bool)
-    ros_service_test = rospy.Service('ui_service_test', Bool)
-
-    ctk.set_appearance_mode("Light")
-    ctk.set_default_color_theme("blue")
-
-    app = ctk.CTk()
-    app.title("ROS with Nucleo Demo")
-
-    style = ttk.Style()
-    style.theme_use("clam")
-
-    main_frame  = ctk.CTkFrame(app)
-    main_frame.grid(row=0,  column=0, columnspan=2, padx=5,pady=5)
-
-    stop_button = ctk.CTkLabel(main_frame, text="ROS Nucleo Demo",font=ctk.CTkFont(size=24, weight="bold"))
-    stop_button.grid(row=0, column=1)
-    start_button = ctk.CTkButton(main_frame, text="Publish Test", command=lambda : ros_publish_test.publish("Hello World"))
-    start_button.grid(row=1,  column=0)
-    stop_button = ctk.CTkButton(main_frame, text="Service Test", command=lambda : ros_service_test.publish("Hello World"))
-    stop_button.grid(row=1, column=2)
-
     def __init__(self):
-        pass
+        rospy.init_node('raspi_ui')
+        rospy.loginfo("raspi_ui node started")
+
+        #ros_service_test = rospy.ServiceProxy('ui_service_test', )
+        self.ros_publish_test = rospy.Publisher('button_b', String, queue_size=10)
+        self.ros_publish_test.publish("Hello World")
+
+        ctk.set_appearance_mode("Light")
+        #ctk.set_default_color_theme("blue")
+
+        self.app = ctk.CTk()
+        self.app.title("ROS with Nucleo Demo")
+
+        style = ttk.Style()
+        style.theme_use("clam")
+
+        main_frame  = ctk.CTkFrame(self.app)
+        main_frame.grid(row=0,  column=0, columnspan=2, padx=5,pady=5)
+
+        def callback__button_b():
+            self.ros_publish_test.publish("Hello World")
+            print("Button B Pressed")
+
+        title = ctk.CTkLabel(main_frame, text="ROS Nucleo Demo",font=ctk.CTkFont(size=24, weight="bold"))
+        title.grid(row=0, column=1)
+        #button_b = ctk.CTkButton(main_frame, text="Publish Test", command=lambda : self.ros_publish_test.publish("Hello World"))
+        button_b = ctk.CTkButton(main_frame, text="Publish Test", command=callback__button_b)
+        button_b.grid(row=1,  column=0)
+        #button_c = ctk.CTkButton(main_frame, text="Service Test", command=lambda : ros_service_test.publish("Hello World"))
+        #button_c.grid(row=1, column=2)
 
     def run(self):
         """
