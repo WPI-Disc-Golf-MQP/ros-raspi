a15b7f81e504bea0ee8938c03c546b3335dd31ed
Benjamin A <yellowjaguar5@gmail.com>
2024-01-15 15:12:52 -0500

    Adjust serial_node

--- raspi_main/src/node_templates.py
+++ raspi_main/src/node_templates.py
@@ -2,19 +2,33 @@
 
 
 from abc import ABC, abstractmethod
+from enum import Enum
 from threading import Timer
 import time
-from typing import Callable, Type
+from typing import Callable
 from typing_extensions import final
 
 import rospy
+from std_msgs.msg import Int8
+
+class NODE_STATUS(Enum):
+    INITIALIZING = 0
+    ONLINE_IDLE = 1
+    REQUEST_IN_PROGRESS = 2
+    REQUEST_COMPLETE = 3
+    INVALID__REPEAT_REQUIRED = 4
+    ERROR = 5
+
+class REQUEST(Enum):
+    INITIALIZING = 0
+    ONLINE_IDLE = 1
+    REQUEST__STANDARD = 2
+    RESET__STANDARD = 3
+    REQUEST__REPEAT = 4
+    REBOOT = 5
 
 
 class measure_node(ABC):
-    def __init__(self):
-        self.enabled = False
-        pass
-
     @abstractmethod
     def get(self):
         pass
@@ -62,12 +76,18 @@ class motion_node(ABC):
         pass
 
 class serial_node(ABC):
-    def __init__(self, watchdog_sub_topic:str="", watchdog_sub_type:Type=Type, timeout:float=2, refresh_rate:float=0.25):
+    def __init__(self, NAME:str, STATUS_TOPIC:tuple[str, type]=("",type(None)),
+                  timeout:float=2, 
+                  refresh_rate:float=0.25):
+        self.name = NAME
+        if STATUS_TOPIC == ("", type(None)):
+            STATUS_TOPIC = (NAME.lower() + "_status", Int8)
         self.last_time:float = 0
-        self.online = lambda : self.last_online is not None and (time.time() - self.last_online) < timeout
+        self.is_online = lambda : self.last_online is not None and (time.time() - self.last_online) < timeout
         self.last_online = False
-        if watchdog_sub_topic is str and watchdog_sub_topic != "":
-            self.watchdog_sub = rospy.Subscriber(watchdog_sub_topic, watchdog_sub_type, self.update)
+        self.status = NODE_STATUS.INITIALIZING
+        if STATUS_TOPIC[0] is str and STATUS_TOPIC[0] != "" and STATUS_TOPIC[1] is not type(None):
+            self.watchdog_sub = rospy.Subscriber(*STATUS_TOPIC, self.update)
         self.refresh_rate = refresh_rate
         self.timer = Timer(refresh_rate, self._check_callbacks)
         self.timer.start()
@@ -80,15 +100,23 @@ class serial_node(ABC):
 
     def update(self, msg):
         self.last_time = time.time()
+        try:
+            self.status = NODE_STATUS[msg.data]
+        except:
+            self.status = NODE_STATUS.ERROR
+            rospy.logerr("[" + self.name + "] Invalid status message received: " + msg.data)
         self._check_callbacks()
 
+    def get_status(self) -> NODE_STATUS:
+        return self.status
+
     def _check_callbacks(self):
-        if not self.last_online and self.online() and self.online_callback is not None:
+        if not self.last_online and self.is_online() and self.online_callback is not None:
             self.online_callback()
-        elif self.last_online and not self.online() and self.offline_callback is not None:
+        elif self.last_online and not self.is_online() and self.offline_callback is not None:
             self.offline_callback()
 
-        self.last_online = self.online()
+        self.last_online = self.is_online()
 
         if self.timer.is_alive: self.timer.cancel()
         self.timer = Timer(self.refresh_rate, self._check_callbacks)
--- raspi_main/src/raspi_hal__led_demo.py
+++ raspi_main/src/raspi_hal__led_demo.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python3
 
-PUB_TOPIC = "led"
-WATCHDOG_TOPIC = "nucleo_led__feedback"
+from std_msgs.msg import Bool, Int8
 
 import rospy
-from std_msgs.msg import Bool
-from node_templates import serial_node
+
+from node_templates import serial_node, NODE_STATUS, REQUEST
+
+FEEDBACK_TOPIC=("led_feedback", Bool)  # bool
+REQUEST_TOPIC=("led_request", Int8) # use REQUEST enum
 
 class hal__led_demo(serial_node):
     def __init__(self):
-        self.led_pub = rospy.Publisher(PUB_TOPIC, Bool, queue_size=10)
-        super().__init__(watchdog_sub_topic=WATCHDOG_TOPIC, watchdog_sub_type=Bool)
+        super().__init__(NAME:="led")
+        self.request_pub = rospy.Publisher(*REQUEST_TOPIC, queue_size=10)
+        self.feedback_sub = rospy.Subscriber(*FEEDBACK_TOPIC, self.feedback_callback)
+        self.led_status = False
+        self.request_pub.publish(REQUEST.INITIALIZING)
 
     def set_led(self, state: bool):
-        self.led_pub.publish(state)
+        self.request_pub.publish(REQUEST.REQUEST__STANDARD if state else REQUEST.ONLINE_IDLE)
+    
+    def feedback_callback(self, msg):
+        self.led_status = msg.data
+
+    def get_led(self) -> bool:
+        return self.led_status
 
 
 if __name__ == '__main__':
@@ -24,8 +35,13 @@ if __name__ == '__main__':
 
     while not rospy.is_shutdown():
         _hal__led_demo.set_led(True)
-        print("Node Online" if _hal__led_demo.online() else "Node Offline")
+        rospy.sleep(0.1)
+        print("Node Online" if _hal__led_demo.is_online() else "Node Offline")
+        print("LED Status: " + "On" if str(_hal__led_demo.get_led()) else "Off")
         rospy.sleep(1)
+
         _hal__led_demo.set_led(False)
-        print("Node Online" if _hal__led_demo.online() else "Node Offline")
+        rospy.sleep(0.1)
+        print("Node Online" if _hal__led_demo.is_online() else "Node Offline")
+        print("LED Status: " + "On" if str(_hal__led_demo.get_led()) else "Off")
         rospy.sleep(1)
