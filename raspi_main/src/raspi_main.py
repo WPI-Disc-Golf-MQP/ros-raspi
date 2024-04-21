6341efa4538f380bd9829b0ea2837d3edefa0f5c
Matt Adam <matthew.adam20@gmail.com>
2024-04-21 16:31:09 -0400

    Update raspi_main.py

--- raspi_main/src/raspi_main.py
+++ raspi_main/src/raspi_main.py
@@ -272,6 +272,7 @@ class raspi_main:
             self.hal__turntable.start() 
         
         elif btn.data == DebuggingButtons.CONVEYOR_START.name:
+            rospy.loginfo("Start!! Conveyor!!");
             self.hal__main_conveyor.start()
 
         elif btn.data == DebuggingButtons.INTAKE_START.name:
